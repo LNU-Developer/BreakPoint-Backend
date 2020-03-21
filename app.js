21d23ec01a082517d8ee661bfb92ce5a0ea900ed
Rickard Marjanovic <rm222jx@student.lnu.se>
2020-03-21 21:40:42 +0100

    Setting upp config against Firebase and testing a request with success

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const firebase = require('firebase')
+const bodyParser = require('body-parser')
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname) + '/.env' })
+
+const app = express()
+app.use(bodyParser.json()) // need to parse HTTP request body
+
+const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+}
+firebase.initializeApp(config)
+
+app.get('/', function (req, res) {
+  res.send('HTTP GET Request')
+  // Insert key,value pair to database
+  firebase.database().ref('/TestMessages').set({ TestMessage: 'GET Request' })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Server running on http://localhost:' + process.env.PORT)
+  console.log('Press Ctrl-C to terminate...\n')
+})
