627570e6db0bb5fb52483c9759b45b5da49dd51c
Rickard Marjanovic <rm222jx@student.lnu.se>
2020-04-10 18:31:52 +0200

    New test

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:4200/api/v1/google/redirect/',
+  callbackURL: 'http://inviflow.n00bs.io/api/v1/google/redirect/',
   scope: ['profile', 'email']
 },
 function (accessToken, refreshToken, profile, done) {
